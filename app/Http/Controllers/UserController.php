094f11e844cc9b0df929f0db43a76246ba020615
nylnessa <nylnessa@gmail.com>
2020-11-21 09:12:34 +0900

    usersテーブルのデータを取得する処理追加

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-
+        $users = User::all();
+        dump($users);
     }
 }
